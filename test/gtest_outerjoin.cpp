1c114138291253e56f209a96652e962d0108875c
sena@s73.work <Sergey>
2024-03-19 22:43:27 +0100

    тесты

--- test/gtest_outerjoin.cpp
+++ test/gtest_outerjoin.cpp
@@ -14,7 +14,44 @@ using std::string;
 
 TEST(simpledb, input1) {
   {
-    EXPECT_EQ(1, 1);
+    SimpleDB sdb;
+
+    EXPECT_EQ(sdb.exec("INSERT A 0 lean"      ), "OK\n");
+    EXPECT_EQ(sdb.exec("INSERT A 0 understand"), "ERR дупликат 0\n");
+    EXPECT_EQ(sdb.exec("INSERT A 1 sweater"   ), "OK\n");
+    EXPECT_EQ(sdb.exec("INSERT A 2 frank"     ), "OK\n");
+    EXPECT_EQ(sdb.exec("INSERT A 3 violation" ), "OK\n");
+    EXPECT_EQ(sdb.exec("INSERT A 4 quality"   ), "OK\n");
+    EXPECT_EQ(sdb.exec("INSERT A 5 precision" ), "OK\n");
+    EXPECT_EQ(sdb.exec("INSERT B 3 proposal"), "OK\n");
+    EXPECT_EQ(sdb.exec("INSERT B 4 example"), "OK\n");
+    EXPECT_EQ(sdb.exec("INSERT B 5 lake"), "OK\n");
+    EXPECT_EQ(sdb.exec("INSERT B 6 flour"     ), "OK\n");
+    EXPECT_EQ(sdb.exec("INSERT B 7 wonder"), "OK\n");
+    EXPECT_EQ(sdb.exec("INSERT B 8 selection"), "OK\n");
+    EXPECT_EQ(sdb.exec("INTERSECTION"),
+              "3,violation,proposal\n"
+              "4,quality,example\n"
+              "5,precision,lake\n"
+              "OK\n");
+    EXPECT_EQ(sdb.exec("SYMMETRIC_DIFFERENCE"),
+              "0,lean,\n"
+              "1,sweater,\n"
+              "2,frank,\n"
+              "6,,flour\n"
+              "7,,wonder\n"
+              "8,,selection\n"
+              "OK\n");
+    EXPECT_EQ(sdb.exec("TRUNCATE A"), "OK\n");
+    EXPECT_EQ(sdb.exec("INTERSECTION"), "OK\n");
+    EXPECT_EQ(sdb.exec("SYMMETRIC_DIFFERENCE"),
+              "3,,proposal\n"
+              "4,,example\n"
+              "5,,lake\n"
+              "6,,flour\n"
+              "7,,wonder\n"
+              "8,,selection\n"
+              "OK\n");
   }
 }
 
